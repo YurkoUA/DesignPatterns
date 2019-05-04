057047646af9486d0573eef1223bdc9287e24181
Yurii Nosach <nosach.yurko@gmail.com>
2019-05-04 14:00:56 +0300

    Added macro command.

---
+++ src/DesignPatterns/DesignPatterns/RemoteControlCommand/Commands/MacroCommand.cs
@@ -0,0 +1,30 @@
+﻿using RemoteControlCommand.Interfaces;
+
+namespace RemoteControlCommand.Commands
+{
+    public class MacroCommand : ICommand
+    {
+        private readonly ICommand[] commands;
+
+        public MacroCommand(params ICommand[] commands)
+        {
+            this.commands = commands;
+        }
+
+        public void Execute()
+        {
+            for (var i = 0; i < commands.Length; i++)
+            {
+                commands[i].Execute();
+            }
+        }
+
+        public void Undo()
+        {
+            for (var i = commands.Length - 1; i >= 0; i--)
+            {
+                commands[i].Undo();
+            }
+        }
+    }
+}
--- src/DesignPatterns/DesignPatterns/RemoteControlCommand/Program.cs
+++ src/DesignPatterns/DesignPatterns/RemoteControlCommand/Program.cs
@@ -15,10 +15,29 @@ namespace RemoteControlCommand
             var tv = new TV();
             var kitchenLight = new Light("Kitchen");
             var yardLight = new Light("Yard");
+            var livingRoomLight = new Light("Living Room");
+            var bathroomLight = new Light("Bathroom");
+            var bedroomLight = new Light("Bedroom");
+
+            var lightOnKitchen = new LightOnCommand(kitchenLight);
+            var lightOnYard = new LightOnCommand(yardLight);
+            var lightOnLivingR = new LightOnCommand(livingRoomLight);
+            var lightOnBathR = new LightOnCommand(bathroomLight);
+            var lightOnBedR = new LightOnCommand(bedroomLight);
+
+            var lightOffKitchen = new LightOffCommand(kitchenLight);
+            var lightOffYard = new LightOffCommand(yardLight);
+            var lightOffLivingR = new LightOffCommand(livingRoomLight);
+            var lightOffBathR = new LightOffCommand(bathroomLight);
+            var lightOffBedR = new LightOffCommand(bedroomLight);
 
             control.SetCommand(Slot.Num0, new TVOnCommand(tv), new TVOffCommand(tv));
-            control.SetCommand(Slot.Num1, new LightOnCommand(kitchenLight), new LightOffCommand(kitchenLight));
-            control.SetCommand(Slot.Num2, new LightOnCommand(yardLight), new LightOffCommand(yardLight));
+            control.SetCommand(Slot.Num1, lightOnKitchen, lightOffKitchen);
+            control.SetCommand(Slot.Num2, lightOnYard, lightOffYard);
+
+            control.SetCommand(Slot.Num3, 
+                new MacroCommand(lightOnKitchen, lightOnYard, lightOnLivingR, lightOnBathR, lightOnBedR), 
+                new MacroCommand(lightOffKitchen, lightOffYard, lightOffLivingR, lightOffBathR, lightOffBedR));
 
             Console.WriteLine(control.ToString() + '\n');
 
@@ -40,6 +59,11 @@ namespace RemoteControlCommand
             Console.WriteLine('\n');
 
             control.OnButtonPressed(Slot.Num4);
+
+            Console.WriteLine('\n');
+
+            control.OnButtonPressed(Slot.Num3);
+            control.UndoButtonPressed();
 
             Console.ReadKey();
         }
